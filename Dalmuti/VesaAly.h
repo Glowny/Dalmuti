2d16c82fc8c413f85e2d0031910faa0aed40d843
Glowny <Nuudelitlautasella@hotmail.com>
2015-04-11 14:51:05 +0300

    debug fileet tossa committaamatta
    Toivottavasti ei riko.

--- Dalmuti/VesaAly.h
+++ Dalmuti/VesaAly.h
@@ -4,12 +4,16 @@ class VesaAly :public Player
 {
 public:
 	VesaAly(std::vector<Card*> hand, std::string playerName);
+
 	Card* AI(std::vector<Card*>* poytakortit, int ylinkortti, int ylimmankortinmaara);
-	Card ChooseCardToPlay(std::vector<Card> PlayableCards);
+
 	std::vector<Card> FindPlayableCards(int cardValue, int cardAmount);
+	Card FindCard(int cardValue);
+
+	Card ChooseCardToPlay(std::vector<Card> PlayableCards);
 	Card FindBiggestValue(std::vector<Card> possibleCards);
 	Card FindLargestAmountOfCards(std::vector<Card> possibleCards);
-	Card FindCard(int cardValue);
+
 	~VesaAly();
 };
 
